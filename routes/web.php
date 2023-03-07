f3ddf7c6963712a83944c243313fcbfc6616aac5
Aurelio Marín Bautista <72474524+vKurama7u7v@users.noreply.github.com>
2023-03-07 13:17:40 -0600

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,23 @@
 <?php
+
 use App\Http\Controllers\PageController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-/*
-Route::get('home', [PageController::class, 'home'])->name('home');
-
-Route::get("blog", [PageController::class, 'blog'])->name('blog');
-
-Route::get('blog/{slug}', [PageController::class, 'post'])->name('post');
-*/
 
 Route::controller(PageController::class)->group(function () {
     Route::get('home', 'home')->name('home');
-
     Route::get("blog", 'blog')->name('blog');
-
-    Route::get('blog/{post:slug}', 'post')->name('post');
+    Route::get('blog/{slug}', 'post')->name('post');
 });
 
-Route::get('buscar', function (Request $request) {
-    return $request->all();
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
